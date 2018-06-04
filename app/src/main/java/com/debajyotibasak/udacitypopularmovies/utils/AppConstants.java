9b4a1476798458de30fd417cd623a12e8445bb2a
Debajyoti <debajyoti.basak@ozoprop.com>
2018-06-04 17:41:56 +0530

    Updated git-ignore and AppConstants

--- app/src/main/java/com/debajyotibasak/udacitypopularmovies/utils/AppConstants.java
+++ app/src/main/java/com/debajyotibasak/udacitypopularmovies/utils/AppConstants.java
@@ -3,7 +3,8 @@ package com.debajyotibasak.udacitypopularmovies.utils;
 public class AppConstants {
 
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
-    public static final String BACKDROP_BASE_PATH = "http://image.tmdb.org/t/p/w300";
+    public static final String POSTER_BASE_PATH = "http://image.tmdb.org/t/p/w185";
+    public static final String BACKDROP_BASE_PATH = "http://image.tmdb.org/t/p/w500";
     public static final String API_KEY_PARAM = "api_key";
     public static final String TABLE_NAME = "movies";
     public static final String DB_NAME = "movies-db";
